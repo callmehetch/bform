70c161d9ab99568cb5b9f9951c7c613c4b754467
callmehetch <callmehetch@gmail.com>
2019-01-16 14:06:40 +0530

    b

--- app.js
+++ app.js
@@ -1,48 +1,60 @@
 var express = require("express");
-var nodemailer = require('nodemailer');
 var app = express();
 var port = 3000;
 var bodyParser = require('body-parser');
 var path = require('path');
+var nodemailer = require('nodemailer');
 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: '54321hemanth@gmail.com',
+        pass: 'hemanth3219'
+    }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 var mongoose = require("mongoose");
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds029828.mlab.com:29828/heroku_qdh3qffk");
+mongoose.connect("mongodb://localhost:27017/data");
 var nameSchema = new mongoose.Schema({
-    
-    
-  rname : String,
-    rroll :String,
-    rgender : String,
-    rphone : Number,
-    rhostel : String,
-    rroom : Number,
-    bname : String,
-    broll : String,
-     bgender : String,
-    bphone : Number,
-    bhostel : String,
-    broom : Number,
-    bdate : Date,
-    bslot : String,
- 
-        name : String,
-    mail : String,
-  
-    
-        
 
-  
-    
-    
+
+    rname: String,
+    rroll: String,
+    rgender: String,
+    rphone: Number,
+    rhostel: String,
+    rroom: Number,
+    bname: String,
+    broll: String,
+    bgender: String,
+    bphone: Number,
+    bhostel: String,
+    broom: Number,
+    bdate: Date,
+    bslot: String,
+    iname: [],
+    phone: [],
+    mail: [],
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: Number,
+        default: 1
+    }
+
+
 });
 
 
@@ -53,20 +65,37 @@ app.get("/", (req, res) => {
 });
 
 
-app.get('/send',function(req,res){
-    var mailOptions={
-        to : req.query.to,
-        subject : req.query.subject,
-        text : req.query.text
-    }
-
-});
-
 app.post("/success", (req, res) => {
+    var rname = req.body.rname;
     var myData = new User(req.body);
+
+
+
     myData.save()
         .then(item => {
-             res.sendFile(__dirname + "/success.html");
+            res.sendFile(__dirname + "/success.html");
+
+
+
+            const mailOptions = {
+                from: 'Hemanth<54321hemanth@gmail.com>',
+                to: req.body.rroll + '@iiitdm.ac.in',
+                subject: 'Re. Celebration request',
+                html: '  <div style="background-color:#377D6A; padding:1px; height: 55px;" ><h3 style="text-align:center;background-color:#377D6A;color:white;margin-top: 15px">Celebration request form</h3></div> <p style="padding-top:10%">Dear ' + rname + ', We have succesfully received your application. This is your final confirmation mail.</p>'
+
+            };
+
+            transporter.sendMail(mailOptions, function (err, info) {
+                if (err)
+                    console.log(err)
+                else
+                    console.log(info);
+            });
+
+
+
+        
+
         })
         .catch(err => {
             res.status(400).send("Unable to save to database");
